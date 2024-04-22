435b4ec09bdc5831538aa7e18facef0812b26f27
IT21822230 <it21822230@my.sliit.lk>
2024-04-22 12:20:15 +0530

    Add initial project files and configurations

---
+++ fitquest/src/test/java/com/regwd08/fitquest/FitquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regwd08.fitquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
