4fcb91ccd0e7c8db9740edf9fb359f3f057ca3f6
Kalmi Dilara <143012202+Kalmid@users.noreply.github.com>
2024-05-06 21:56:51 +0530

    Add files via upload

---
+++ FitnessdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.fitnessdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessdemoApplication.class, args);
+		
+	}
+
+}
