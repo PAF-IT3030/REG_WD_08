8fb83e08dcb0359489377289eeb9d4072d49b7d1
Kalmid <kalmid.98@gmail.com>
2024-05-09 12:13:54 +0530

    Edit

--- fitness/MediaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fitnessdemo.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.fitnessdemo.Entity.Media;
-@Repository
-public interface MediaRepository extends JpaRepository<Media, Integer> {
-}
