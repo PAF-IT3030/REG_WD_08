03fbbc30081d434fac0f2b1e997ef8bad15f4664
Kalmid <kalmid.98@gmail.com>
2024-05-09 12:12:10 +0530

    Edit

--- fitness/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.fitnessdemo.Service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- fitness/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fitnessdemo.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.fitnessdemo.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    //User findByUsername(String username);
-}
