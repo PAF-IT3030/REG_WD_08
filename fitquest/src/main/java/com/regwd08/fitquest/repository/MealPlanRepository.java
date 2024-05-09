d3f7fa6bfc49e1058feffc69df0adae10fe72893
Kalmi Dilara <143012202+Kalmid@users.noreply.github.com>
2024-05-09 11:11:48 +0530

    Delete directory

--- fitquest/src/main/java/com/regwd08/fitquest/repository/MealPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.regwd08.fitquest.repository;
-import com.regwd08.fitquest.model.MealPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealPlanRepository extends JpaRepository<MealPlan, Long> {
-
-}
